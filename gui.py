9f31a049793a5e1a4682df06aa4fd2b23728b1b9
schel337 <57027835+schel337@users.noreply.github.com>
2020-05-24 15:52:11 -0500

    Matrix inputs and eigenvalue formatting
    Made matrix inputs use a matrix of entries and added support for eigenvalue plotting.

--- gui.py
+++ gui.py
@@ -26,15 +26,17 @@ def calculate():
 	Calculates new dynamics of the system
 	"""
 	T = int(time_var.get())
-	A = parse_matrix(A_var.get())
-	B = parse_vector(B_var.get())
-	x0 = parse_vector(x0_var.get())
+	A = A_var.get()#parse_matrix(A_var.get())
+	print(A)
+	B = B_var.get()#parse_vector(B_var.get())
+	print(B)
+	x0 = x0_var.get()#parse_vector(x0_var.get())
+	print(x0)
 	if contin_var.get():
 		s = int(sampling_var.get())
 		return np.linspace(0,T,num=s), matrix_exp_sys(A,B,x0,T,s)#contin_lin_sys(A,B,x0,T,s)
 	else:	
 		return np.array((range(T+1))), disc_lin_sys(A,B,x0,T)
-
 
 def plot_static():
 	try:
@@ -47,7 +49,6 @@ def plot_static():
 		canvas.draw()
 	except ValueError:
 		pass
-
 	
 def plot_animated():
 	try:
@@ -97,29 +98,24 @@ def plot_animated_plane():
 		updater(0)
 	except ValueError:
 		pass	
-		
+
+def plot_eigenvals():
+	#TODO: changing projection for polar
+	A = A_var.get()
+	evals, evecs = np.linalg.eig(A)
+	axs.clear()
+	r = 1.414 * np.max(np.sqrt(evals.real**2 + evals.imag**2))
+	axs.set_xlim(left=-r,right=r)
+	axs.set_ylim(bottom=-r,top=r)
+	axs.plot(evals.real, evals.imag, 'bo')
+	canvas.draw()
 	
+
 def randomize():
-	try:
-		x0 = parse_vector(x0_var.get())
-		n = x0.shape[0]
-		A = ""
-		for i in range(n):
-			for j in range(n):
-				A += str(round(random.uniform(-1,1),3))
-				if j + 1 < n:
-					A += ','
-			if i + 1 < n:
-				A += ';'
-		A_var.set(A)
-		B = ""
-		for i in range(n):
-			B += str(round(random.uniform(-1,1),3))
-			if i + 1 < n:
-				B += ','
-		B_var.set(B)
-	except ValueError:
-		pass
+	x0 = x0_var.get()
+	n = x0.shape[0]
+	A_var.set(np.array([[round(random.uniform(-1,1),3) for _ in range(n)] for _ in range(n)]))
+	B_var.set(np.array([[round(random.uniform(-1,1),3)] for _ in range(n)]))
 		
 	
 		
@@ -127,7 +123,7 @@ input_frame = ttk.Frame(root, borderwidth=5, relief="sunken")
 input_frame.grid(column=0, row=1, sticky="nsew")
 
 #TODO: add var for plot type and have that one handle plotting things
-plot_type_var = entry_var("Plot", tk.StringVar(), input_frame, default='static')
+plot_type_var = ComboboxVar("Plot Type", tk.StringVar(), input_frame, opts=('static','animate','plane','eigenvalues'), val='static')
 def plot():
 	plotter = plot_type_var.get()
 	if plotter == 'static':
@@ -136,17 +132,30 @@ def plot():
 		plot_animated()
 	elif plotter == 'plane':
 		plot_animated_plane()
+	elif plotter == 'eigenvalues':
+		plot_eigenvals()
 ttk.Button(input_frame, text="Plot", command=plot).pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 #ttk.Button(input_frame, text="Animate", command=plot_animated).pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 ttk.Button(input_frame, text="Randomize", command=randomize).pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
-A_var  = entry_var("A",tk.StringVar(), input_frame, default="0.5,-0.5;\r 0.5,0.5")
-B_var = entry_var("B",tk.StringVar(), input_frame, default = "1,1")
-x0_var = entry_var("x0",tk.StringVar(), input_frame, default = "1,1")
-time_var = entry_var("T",tk.StringVar(), input_frame, default = "20")
+sys_frame = ttk.Frame(input_frame)
+sys_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+dim_var = EntryVar("Dim", tk.StringVar(), sys_frame, val="2")
+A_var  = MatrixEntryVar("A", (2,2), sys_frame, val=[[0.5,-0.5],[0.5,0.5]]) #EntryVar("A",tk.StringVar(), input_frame, val="0.5,-0.5;\r 0.5,0.5")
+B_var = MatrixEntryVar("B", (2,1), sys_frame, val=[[1],[1]]) #EntryVar("B",tk.StringVar(), input_frame, val = "1,1")
+x0_var = MatrixEntryVar("x0", (2,1), sys_frame, val=[[1],[1]]) #EntryVar("x0",tk.StringVar(), input_frame, val = "1,1")
+time_var = EntryVar("T",tk.StringVar(), input_frame, val = "20")
+def dim_change(*args):
+	n = dim_var.get()
+	if n:
+		n = int(n)
+		A_var.reshape((n,n))
+		B_var.reshape((n,1))
+		x0_var.reshape((n,1))
+dim_var.trace("w", dim_change)
 
-contin_var = bool_var("Continuous", tk.BooleanVar(), input_frame, default=False)
-sampling_var = entry_var("Samples", tk.StringVar(), input_frame, show=False, default="100")
+contin_var = BoolVar("Continuous", tk.BooleanVar(), input_frame, val=False)
+sampling_var = EntryVar("Samples", tk.StringVar(), input_frame, show=False, val="100")
 #Possibly remove, use plot button as main method
 def toggle_contin():
 	if contin_var.get():
--- utils.py
+++ utils.py
@@ -19,17 +19,22 @@ def parse_vector(vector_string):
 	Return np array
 	"""
 	return np.array([float(x) for x in vector_string.split(',')])
-	
-class var_wrapper:
-	def __init__(self, name, var, frame, default=None):
+
+class VarWrapper:
+	def __init__(self, name,  var, parent, **kwargs):
 		self.var = var
 		self.name = name
-		self.frame = frame
-		self.label = ttk.Label(self.frame, text=self.name)
-		if default != None:
-			self.set(default)
-		print("Initalized ", name, " with val ", default)
+		self.parent = parent
+		self.label = ttk.Label(self.parent, text=self.name)
+		if 'val' in kwargs:
+			self.set(kwargs['val'])
+		if 'show' not in kwargs or kwargs['show']:
+			self.show()
+		print("Initalized ", name, "with params ", kwargs)
 		
+	def trace(self, mode, callback):
+		self.var.trace(mode, callback)
+	
 	def set(self, val):
 		self.var.set(val)
 		
@@ -50,17 +55,86 @@ class var_wrapper:
 	def bind_command(self, command):
 		self.input_widget.configure(command=command)
 		
-class entry_var(var_wrapper):
-	def __init__(self, name, var, frame, show=True, default=None):
-		super().__init__(name, var, frame, default=default)
-		self.input_widget = ttk.Entry(self.frame, width=DEFAULT_WIDTH, textvariable=var)
-		if show:
+class EntryVar(VarWrapper):
+	def __init__(self, name, var, parent, **kwargs):
+		self.input_widget = ttk.Entry(parent, width=DEFAULT_WIDTH, textvariable=var)
+		super().__init__(name, var, parent, **kwargs)
+
+class BoolVar(VarWrapper):
+	def __init__(self, name, var, parent, **kwargs):
+		self.input_widget = ttk.Checkbutton(parent, width=DEFAULT_WIDTH, variable=var)
+		super().__init__(name, var, parent, **kwargs)
+		
+			
+class ComboboxVar(VarWrapper):
+	def __init__(self, name, var, parent, **kwargs):
+		assert 'opts' in kwargs, "Comboboxes need options"
+		self.input_widget = ttk.Combobox(parent, textvariable=var, values=kwargs.pop('opts'))
+		super().__init__(name, var, parent, **kwargs)
+
+#Not subclass of VarWrapper, this will manage 2d arrays of vars (or 1d as a special case)
+class MatrixEntryVar():
+	def __init__(self, name, shape, parent, **kwargs):
+		self.name = name
+		self.parent = parent
+		self.label = ttk.Label(parent, text=self.name)
+		self.frame = ttk.Frame(parent)
+		self.m, self.n = shape[0], shape[1]
+		self.vars = [[tk.StringVar() for _ in range(self.n)] for _ in range(self.m)]
+		self.entries = [[ttk.Entry(self.frame, width=DEFAULT_WIDTH//2, textvariable=self.vars[i][j]) for j in range(self.n)] for i in range(self.m)]
+		if 'val' in kwargs:
+			#[[self.vars[i][j].set(kwargs['val'][i][j]) for j in range(self.m)] for i in range(self.n)]
+			self.set(kwargs['val'])
+		if 'show' not in kwargs or kwargs['show']:
 			self.show()
 	
+	def reshape(self, shape):
+		self.hide()
+		for i in range(self.m):
+			for j in range(self.n):
+				if i >= shape[0] or j >= shape[1]:
+					self.entries[i][j].destroy()
+		temp_vars, temp_entries = np.empty(shape, dtype=object), np.empty(shape, dtype=object)
+		for i in range(shape[0]):
+			for j in range(shape[1]):
+				if i < self.m and j < self.n:
+					temp_vars[i][j] = self.vars[i][j]
+					temp_entries[i][j] = self.entries[i][j]
+				else:
+					temp_vars[i][j] = tk.StringVar()
+					temp_entries[i][j] = ttk.Entry(self.frame, width=DEFAULT_WIDTH//2, textvariable=temp_vars[i][j])
+					temp_vars[i][j].set("0")
+		self.vars = temp_vars #[[tk.StringVar() for _ in range(self.m)] for _ in range(self.n)]
+		self.entries = temp_entries #[[ttk.Entry(self.frame, width=DEFAULT_WIDTH//2, textvariable=self.vars[i][j]) for j in range(self.n)] for i in range(self.m)]
+		self.m, self.n = shape[0], shape[1]
+		self.show()
 
-class bool_var(var_wrapper):
-	def __init__(self, name, var, frame, show=True, default=None):
-		super().__init__(name, var, frame, default=default)
-		self.input_widget = ttk.Checkbutton(self.frame, width=DEFAULT_WIDTH, variable=self.var)
-		if show:
-			self.show()
+	def set(self, val):
+		val = np.array(val)
+		if val.shape[0] != self.m or val.shape[1] != self.n:
+			print(val.shape)
+			print(self.m, self.n)
+			self.resize(val.shape)
+		[[self.vars[i][j].set(val[i][j]) for j in range(self.n)] for i in range(self.m)]
+		
+	def get(self):
+		try:
+			if self.n == 1:
+				return np.array([float(self.vars[i][0].get()) for i in range(self.m)])
+			elif self.m == 1:
+				return np.array([float(self.vars[0][j].get()) for i in range(self.n)])
+			else:
+				return np.array([[float(self.vars[i][j].get()) for j in range(self.m)] for i in range(self.n)])
+		except ValueError:
+			print("error fetching: "+self.name)		
+			
+	def show(self):
+		self.label.pack(side=tk.LEFT)
+		self.frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+		[[self.entries[i][j].grid(row=i,column=j) for j in range(self.n)] for i in range(self.m)]
+		
+	def hide(self):
+		self.label.pack_forget()
+		[[self.entries[i][j].grid_forget() for j in range(self.n)] for i in range(self.m)]
+		self.frame.pack_forget()
+		
